0e4bda1376d76598fe494822e121da54df3d134d
Suhani <144649408+suhanidubey123@users.noreply.github.com>
2024-01-23 13:07:42 +0530

    Add files via upload

---
+++ febonacci_series.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class febonacci_series {
+    
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.print("enter any number:");
+        int num=obj.nextInt();
+        int i,n1,n2=1,n3=0;
+        for(i=1;i<=num;i++);
+        {
+            System.out.print(n3+" ");
+            n1=n2;
+            n2=n3;
+            n3=n1+n2;}
+    }}
+    
+  
+
+
+        
+        
+        
+        
+        
+
+    
