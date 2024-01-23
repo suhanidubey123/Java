46eb5610289fd27bf703e5140f69788617360dc0
Suhani <144649408+suhanidubey123@users.noreply.github.com>
2024-01-23 13:08:42 +0530

    Add files via upload

---
+++ number_1.java
@@ -0,0 +1,16 @@
+
+public class number_1 {
+    
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=5;j++)
+            {if(i==j)
+            {continue;}
+                System.out.print("\n"+i+" "+j);
+            }
+        }
+    
+}
+}
